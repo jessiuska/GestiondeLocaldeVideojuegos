5a1debc3a0432ca23ebd109680784f678a8c0d26
Pablo Falcon <pablofal.pf@gmail.com>
2021-11-30 20:45:52 -0300

    lpm

--- src/main/java/egg/GestionVideojuegos/entidades/Tarjeta.java
+++ src/main/java/egg/GestionVideojuegos/entidades/Tarjeta.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package egg.GestionVideojuegos.entidades;
 
 import java.time.LocalDate;
@@ -16,10 +12,6 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-/**
- *
- * @author Administrador
- */
 @Entity 
 @Getter
 @Setter
